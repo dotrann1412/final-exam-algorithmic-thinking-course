f13c6592e492a76d2a98c5d25491a950dfe44256
dotrann1412 <dotrann.1412@gmail.com>
2023-01-04 00:31:25 +0700

    update solution

--- solutions/Problem03/Solution02.cpp
+++ solutions/Problem03/Solution02.cpp
@@ -20,13 +20,66 @@ const ll MOD = 1e9 + 7;
 const db eps = 1e-5;
 const db pi = acos(-1);
 
+bool operator == (const pii& a, const pii& b) {
+	return a.fi == b.fi;
+}
+
+bool operator < (const pii& a, const pii& b) {
+	return a.fi < b.fi;
+}
+
+bool operator > (const pii& a, const pii& b) {
+	return a.fi > b.fi;
+}
+
+bool operator <= (const pii& a, const pii& b) {
+	return a.fi <= b.fi;
+}
+
+bool operator >= (const pii& a, const pii& b) {
+	return a.fi >= b.fi;
+}
+
+pii operator - (const pii& a) {
+	return {-a.fi, a.se};
+}
+
 void solve() {
-	
+	int n, m; cin >> n >> m;
+	vector<pii> a(n), b(m);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i].fi, a[i].se = i;
+	}
+
+	for (int i = 0; i < m; ++i) {
+		cin >> b[i].fi, b[i].se = i;
+	}
+
+	sort(all(a)), sort(all(b));
+
+	int res_a = 0, res_b = 0;
+	for (int i = 0; i < n; ++i) {
+		int k = max(0, lower_bound(all(b), -a[i]) - b.begin());
+		if (abs(a[res_a].fi + b[res_b].fi) > abs(a[i].fi + b[k].fi)) {
+			res_a = i, res_b = k;
+		}
+	}
+
+	for (int i = 0; i < m; ++i) {
+		int k = max(0, lower_bound(all(a), -b[i]) - a.begin());
+		if (abs(a[res_a].fi + b[res_b].fi) > abs(b[i].fi + a[k].fi)) {
+			res_a = k, res_b = i;
+		}
+	}
+
+
+	cout << a[res_a].se + 1 << ' ' << b[res_b].se + 1 << '\n';
+
 }
 
 int main() {
 	fastio();
-	int t = 1; cin >> t;
+	int t = 1; // cin >> t;
 	while(t--) solve();
 	return 0;
 }
--- solutions/Problem05/Solution02.cpp
+++ solutions/Problem05/Solution02.cpp
@@ -21,12 +21,43 @@ const db eps = 1e-5;
 const db pi = acos(-1);
 
 void solve() {
-	
+	string s, t; cin >> s >> t;
+	for (auto& x: s) 	
+		if (x >= 'A' && x <= 'Z') x += 32;
+		
+	for (auto& x: t) 
+		if (x >= 'A' && x <= 'Z') x += 32;
+
+	int64_t pows = 1;
+	const int BASE = 29;
+	for (int i = 0; i < t.size(); 
+		(pows *= BASE) %= MOD, ++i
+	);
+
+	vector<int64_t> hashs(s.size(), s[0] - 'a');
+	for (int i = 1; i < s.size(); ++i) {
+		hashs[i] = (hashs[i - 1] * BASE + s[i] - 'a') % MOD;
+	}
+
+	int64_t hasht = t[0] - 'a';
+	for (int i = 1; i < t.size(); ++i) {
+		((hasht *= BASE) += t[i] - 'a') %= MOD;
+	}
+
+	auto get = [&] (int left, int right) {
+		return (hashs[right] - (left ? hashs[left - 1] : 0) * pows + MOD * MOD) % MOD;
+	};
+
+	for (int i = 0; i + t.size() - 1 < s.size(); ++i) {
+		if (hasht == get(i, i + t.size() - 1)) {
+			cout << i + 1 << ' ';
+		}
+	}
 }
 
 int main() {
 	fastio();
-	int t = 1; cin >> t;
+	int t = 1; // cin >> t;
 	while(t--) solve();
 	return 0;
 }
