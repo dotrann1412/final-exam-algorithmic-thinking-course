262c2553c483da6b4494c1f046948b3983997b9a
dotrann1412 <dotrann.1412@gmail.com>
2023-01-13 04:07:59 +0700

    update runner

--- solutions/run.sh
+++ solutions/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-run_dir=$1
-sol_file=${2:-Solution}
+# ./run.sh 01 02 --> run 2nd solution of problem 01
+source_file=./Problem${1}/Solution${2}.cpp
 exe_file=main
 
-g++ -std=c++2a $run_dir/$sol_file.cpp -o $exe_file -fsplit-stack -Wall -Wextra -Wshadow
+g++ -std=c++2a ${source_file} -o $exe_file -fsplit-stack -Wall -Wextra -Wshadow
 cat test.inp | ./$exe_file
